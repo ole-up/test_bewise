b69a3ef7d45ee9fef5d587a13099495b25068f26
Ole_Up <2987026@mail.ru>
2022-05-04 00:29:29 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,21 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+from app.questions.api import router
+
+app = FastAPI(
+    title="Questions API",
+    version="0.0.1"
+)
+
+app.include_router(router)
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse("/docs")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
