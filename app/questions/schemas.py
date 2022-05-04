9607d7521a618dd72e799e30f19462c196449d79
Ole_Up <2987026@mail.ru>
2022-05-04 23:55:21 +0300

    Create Question and QuestionRequest schemas

---
+++ app/questions/schemas.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ValidationError, validator
+
+class QuestionRequest(BaseModel):
+    questions_num: int = Field(description="Количество запрашиваемых вопросов")
+
+class Question(BaseModel):
+    id: int
+    question: str
+    answer: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
